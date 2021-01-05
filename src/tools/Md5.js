b37439e8b541e67ec15941457af317a423d9c4a9
Nicholas Wech <nwech@renderosity.com>
2021-01-04 20:29:50 -0600

    toggle multiline input vs one input per line for md5

--- src/tools/Md5.js
+++ src/tools/Md5.js
@@ -8,6 +8,7 @@ export default function Md5Tool(){
 
     const [input, setInput] = useState('')
     const [mode, setMode] = useState('encode')
+    const [multiline, setMultiline] = useState(true)
 
 
     return (
@@ -18,7 +19,7 @@ export default function Md5Tool(){
             </label>
             <div className='transition'>&#x21E9;</div>
             <div className="controls">
-                <div className='mode'>
+                <div className='control'>
                     <label>
                         <input type='radio' name='mode' onChange={modeSelect} value='encode' checked={mode === 'encode'}/>
                         Encode
@@ -26,6 +27,16 @@ export default function Md5Tool(){
                     <label>
                         <input type='radio' name='mode' onChange={modeSelect} value='decode' checked={mode === 'decode'}/>
                         Decode
+                    </label>
+                </div>
+                <div className='control'>
+                    <label>
+                        <input type='radio' name='newlines' onChange={multilineSelect} value='multi' checked={multiline}/>
+                        Multiline input
+                    </label>
+                    <label>
+                        <input type='radio' name='newlines' onChange={multilineSelect} value='single' checked={!multiline}/>
+                        One per line
                     </label>
                 </div>
             </div>
@@ -44,12 +55,24 @@ export default function Md5Tool(){
 
     function solve(){
         if(!input)  return ''
-        if(mode === 'encode')  return md5(input)
-        else                   return 'not supported yet'
+        if(mode === 'encode'){
+            if (multiline) {
+                return md5(input)
+            } else {
+                return input.split('\n').map(line => md5(line)).join('\n')
+            }
+        }
+        else{
+            return 'not supported yet'
+        }
     }
 
     function modeSelect(e){
         setMode(e.target.value)
+    }
+
+    function multilineSelect(e){
+        setMultiline(e.target.value === 'multi')
     }
 
 }
